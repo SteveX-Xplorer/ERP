ffa767dbeb577704be1515130215802752635d61
StevefromSpace <iamstevefromspace@gmail.com>
2026-02-20 22:41:21 +0530

    Changed css for tailwind v4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App(){
       <div className="flex flex-col min-h-screen bg-[#0f172a]">
         <Navbar />
 
-        <main className="flex-grow flex flex-col">
+        <main className="grow flex flex-col">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
