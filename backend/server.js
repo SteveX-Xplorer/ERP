d2e8fcf9c7f14bda28169632c14223f9982ed15d
StevefromSpace <iamstevefromspace@gmail.com>
2026-03-06 09:32:08 +0530

    feat: changeed login to get details from database

--- backend/server.js
+++ backend/server.js
@@ -31,37 +31,14 @@ const FACULTY_USER={
     roles: ["admin", "faculty"]
 }
 
-app.post('/api/login', async(req,res)=>{
-    const{email, password}=req.body;
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const user = result.rows[0];
 
-    try{
-        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-
-
-        if(result.row.length>0){
-            const user=result.row[0];
-        }
-        
-            if(user.password===password){
-                res.status(200).json({
-                    success: true,
-                    role:user.roles
-                });
-            }else{
-                res.status(401).json({
-                    success: true;
-                    message:"Wrong Password";
-                })
-            }
-    }
-
-    /*if(email===FACULTY_USER.email && password===FACULTY_USER.password){
-        res.status(200).json({
-            success: true, 
-            message: "Login successful",
-            roles: FACULTY_USER.roles
-    });
+    if (user && user.password === password) {
+        res.status(200).json({ success: true, roles: user.roles });
     } else {
-        res.status(401).json({success: false, message: "Invalid Credential"});
-    }*/
+        res.status(401).json({ success: false, message: "Invalid credentials" });
+    }
 });
